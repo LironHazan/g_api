889a9e6b336940942fc65c62254d89edd58c0bd0
LironH <lironenk@gmail.com>
2023-01-28 20:14:02 +0200

    tiny refactor (#2)

--- apps/appi/src/app/App.tsx
+++ apps/appi/src/app/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { AppiComponents, AppMainLayout, ReposPage } from '@g_api/appi-components';
+import { AppiPage, AppMainLayout, ReposPage } from '@g_api/appi-components';
 
 export function App() {
   return (
     <Routes>
         <Route path="/" element={<AppMainLayout />}>
           <Route path="repos" element={<ReposPage/>} />
-          <Route path="appi" element={<AppiComponents />} />
+          <Route path="appi" element={<AppiPage />} />
         </Route>
     </Routes>
   );
