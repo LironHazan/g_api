b8923811da96b9914b80456a4d7cf64db991ba62
lironh <lironenk@gmail.com>
2023-01-29 10:11:43 +0200

    tiny refactor

--- libs/appi-components/src/index.ts
+++ libs/appi-components/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/components/SearchField';
 export * from './lib/pages/ReposPage';
 export * from './lib/pages/AppiPage';
 export * from './lib/AppMainLayout';
--- libs/appi-components/src/lib/components/repos/ReposTable.tsx
+++ libs/appi-components/src/lib/components/repos/ReposTable.tsx
@@ -4,6 +4,7 @@ import { tokens } from '../../theme';
 import DataTable from '../common/DataTable';
 import { GridEventListener } from '@mui/x-data-grid';
 import { ApolloError } from '@apollo/client/errors';
+import { useRepos } from '../../hooks/useRepos';
 
 const userTableStyles = {
   height: '650px',
@@ -12,10 +13,10 @@ const userTableStyles = {
 interface ReposType {
   loading: boolean,
   error: ApolloError | undefined,
-  repos: { id: string, name: string, url: string, description: string }[]
+  repos: { id: string, name: string, url: string, description: string }[] | undefined
 }
 
-export function Repos({ repos }: ReposType) {
+function ReposTable({ repos }: ReposType) {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
@@ -56,28 +57,37 @@ export function Repos({ repos }: ReposType) {
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      <Box m="20px">
-        <Box
-          m="40px 0 0 0"
-          height="75vh"
-          sx={{
-            "& .MuiDataGrid-columnHeaders": {
-              color: colors.blueAccent[300],
-            },
-            "& .MuiCheckbox-root": {
-              color: `${colors.blueAccent[200]} !important`,
-            },
-          }}
-        >
-          <DataTable
-            rows={repos}
-            columns={columns}
-            loading={!repos.length}
-            sx={userTableStyles}
-            onRowClick={handleRowClick}
-          />
+      { !repos ? <div> Search for a github user </div> :
+        <Box m="20px">
+          <Box
+            m="40px 0 0 0"
+            height="75vh"
+            sx={{
+              "& .MuiDataGrid-columnHeaders": {
+                color: colors.blueAccent[300],
+              },
+              "& .MuiCheckbox-root": {
+                color: `${colors.blueAccent[200]} !important`,
+              },
+            }}
+          >
+            <DataTable
+              rows={repos}
+              columns={columns}
+              // loading={!repos?.length}
+              sx={userTableStyles}
+              onRowClick={handleRowClick}
+            />
+          </Box>
         </Box>
-      </Box>
+      }
     </Box>
+  );
+}
+
+export function UserReposTable({ username }: { username: string }) {
+  const { loading, error, repos } = useRepos(username)
+  return (
+    <ReposTable error={error} loading={loading} repos={repos}/>
   );
 }
