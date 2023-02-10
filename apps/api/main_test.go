0037e39b4791cec8eec71a0f8f4e5f485c754851
lironh <lironenk@gmail.com>
2023-02-10 17:57:07 +0200

    fixed runs

--- apps/api/main_test.go
+++ apps/api/main_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestScrapePage(t *testing.T) {
+func TestAsyncHashUserEmail(t *testing.T) {
+	// assert that the channel is closed
+	t.Run("Channel is closed", func(t *testing.T) {
+		fmt.Println("running nothing")
+	})
 
 }
