aa23728bb4ab5e48fd552c37fe752367d10160f0
LironH <lironenk@gmail.com>
2023-02-10 19:01:08 +0200

    add go app (#9)
    * added a go app with an async pattern
    
    * removed redundant file
    
    * removed redundant file
    
    * fixed runs
    
    * Merge branch 'master' of https://github.com/LironHazan/g_api into add-go-app
    
    # Conflicts:
    #	apps/api/main.go
    #	apps/api/main_test.go

---
+++ apps/api/user/dummy_user.go
@@ -0,0 +1,89 @@
+package user
+
+import (
+	"crypto/sha512"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func HashUserEmail(id int) (string, error) {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/users/" + strconv.Itoa(id))
+	if err != nil {
+		return "", err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	user := User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		return "", err
+	}
+	// This is ugly :) but go's performance are too good and I'm trying to make a blocking computation
+	return hash(hash(hash(hash(hash(hash(hash(hash(hash(hash(user.Email)))))))))), nil
+}
+
+func AsyncHashUserEmail(id int) chan interface{} {
+	start := time.Now()
+	ch := make(chan interface{}, 1)
+	go func() {
+		_start := time.Now()
+		user, err := HashUserEmail(id)
+		elapsed := time.Since(_start)
+		fmt.Println(elapsed)
+		if err != nil {
+			ch <- errors.New("failed")
+		} else {
+			ch <- user
+		}
+	}()
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
+	return ch
+}
+
+func MatchHash(result interface{}) {
+	switch v := result.(type) {
+	case string:
+		fmt.Println(v)
+	case error:
+		fmt.Println(v)
+	}
+}
+
+func hash(input string) string {
+	// Add a random salt to the input string
+	rand.Seed(time.Now().UnixNano())
+	salt := strconv.Itoa(rand.Int())
+	input = input + salt
+
+	// Perform SHA-512 hash on the input
+	hash := sha512.New()
+	hash.Write([]byte(input))
+	sha := hex.EncodeToString(hash.Sum(nil))
+
+	// Add the salt to the end of the hash
+	sha = sha + salt
+
+	// Perform another round of SHA-512 hash
+	hash = sha512.New()
+	hash.Write([]byte(sha))
+	sha = hex.EncodeToString(hash.Sum(nil))
+
+	// Return the final hash string
+	return sha
+}
