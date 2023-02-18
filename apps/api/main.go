d0e82f06d7a3d20e75d97f27c31f44555fd02664
LironH <lironenk@gmail.com>
2023-02-18 21:03:08 +0200

    adding gorm + dev container for postgres (#11)
    * adding gorm + dev container for postgres
    
    * dogolangci-lint run
    
    * restruct

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	routes "g_api/libs/api/go-api/cmd"
+	"fmt"
+	"g_api/libs/common-db/db"
+	routes "g_api/libs/go-api"
+	"g_api/libs/go-api/pkg/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
 )
 
-func startServer() {
+func startServer(db *gorm.DB) {
 	app := fiber.New()
-	routes.ResolveRoutes(app)
+	routes.ResolveRoutes(app, db)
 	err := app.Listen(":3000")
 	if err != nil {
 		return
@@ -15,5 +20,30 @@ func startServer() {
 }
 
 func main() {
-	startServer()
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	config := db.Config{
+		Host:     viper.GetString("DB_HOST"),
+		Port:     viper.GetString("DB_PORT"),
+		Password: viper.GetString("DB_PASS"),
+		User:     viper.GetString("DB_USER"),
+		SSLMode:  viper.GetString("DB_SSLMODE"),
+		DBName:   viper.GetString("DB_NAME"),
+	}
+
+	fmt.Println(config.Host)
+
+	if _db, err := db.Connection(config); err == nil {
+		err := models.MigrateDummyUsers(_db)
+		if err != nil {
+			return
+		}
+		startServer(_db)
+	}
+
 }
